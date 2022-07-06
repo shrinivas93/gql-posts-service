619df4ba957670fd13a2131c5b6273e20a21cb67
Shrinivas Shukla <ssshukla1993@gmail.com>
2022-07-07 00:59:48 +0530

    Added @key & reference resolver testing

--- src/main/java/com/gql/gqlpostsservice/GraphQLConfig.java
+++ src/main/java/com/gql/gqlpostsservice/GraphQLConfig.java
@@ -1,16 +1,50 @@
 package com.gql.gqlpostsservice;
 
 import com.apollographql.federation.graphqljava.Federation;
+import com.apollographql.federation.graphqljava._Entity;
+import com.gql.gqlpostsservice.domain.Post;
+import com.gql.gqlpostsservice.domain.PostFilter;
+import com.gql.gqlpostsservice.resolver.QueryResolver;
 import graphql.kickstart.tools.SchemaParser;
 import graphql.schema.GraphQLSchema;
+import graphql.schema.idl.SchemaPrinter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Configuration
 public class GraphQLConfig {
 
     @Bean
-    public GraphQLSchema customSchema(SchemaParser schemaParser){
-        return Federation.transform(schemaParser.makeExecutableSchema()).build();
+    public GraphQLSchema customSchema(SchemaParser schemaParser, QueryResolver queryResolver) {
+        return Federation.transform(schemaParser.makeExecutableSchema())
+                .fetchEntities(env -> env.<List<Map<String, Object>>>getArgument(_Entity.argumentName)
+                        .stream()
+                        .map(values -> {
+                            if ("Post".equals(values.get("__typename"))) {
+                                final Object id = values.get("id");
+                                if (id instanceof Integer) {
+                                    return queryResolver.posts(new PostFilter((Integer) id));
+                                }
+                            }
+                            return null;
+                        })
+                        .collect(Collectors.toList()))
+                .resolveEntityType(env -> {
+                    final Object src = env.getObject();
+                    if (src instanceof Post) {
+                        return env.getSchema().getObjectType("Post");
+                    }
+                    return null;
+                }).build();
     }
+
+    @Bean
+    public SchemaPrinter customSchemaPrinter(){
+        return new SchemaPrinter(SchemaPrinter.Options.defaultOptions().includeDirectives(true).includeDirectiveDefinitions(true).includeSchemaDefinition(true).includeIntrospectionTypes(true));
+    }
+
 }
